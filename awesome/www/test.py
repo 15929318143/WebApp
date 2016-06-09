87549464e4bd35258db552072bc60ecb79d0e6b8
root <1010346494@qq.com>
2016-06-09 22:09:32 +0800

    完成orm和models的编写，并附有详细注释，和测试代码

---
+++ awesome/www/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import orm
+import asyncio
+import sys
+from models import User, Blog, Comment
+
+@asyncio.coroutine
+def test(loop):
+    yield from orm.create_pool(loop=loop, host='localhost', port='3306', user='www-data', password='www-data', db='awesome')
+    u = User(name='test77', email='test77@test.com', password='test', image='about:blank')
+    yield from s.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
+if loop.is_closed():
+    sys.exit(0)
